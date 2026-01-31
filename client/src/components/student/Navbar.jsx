319b15fe0131c688657f23c77284a12c775ee392
shanto <shantokumar22@gmail.com>
2026-02-01 02:50:08 +0600

    add student Navbar component with Clerk authentication, navigation links, and responsive design.

--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -5,8 +5,8 @@ import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 
 const Navbar = () => {
   const isCourseListPage = location.pathname.includes("/course-list");
-  const {openSignIn} = useClerk();
-  const {user} = useUser();
+  const { openSignIn } = useClerk();
+  const { user } = useUser();
   return (
     <div
       className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 py-4 border-b border-gray-500 ${isCourseListPage ? "bg-white" : "bg-cyan-100/70"}`}
@@ -18,22 +18,27 @@ const Navbar = () => {
       />
       <div className="hidden md:flex items-center gap-5 text-gray-700">
         <div className="flex items-center gap-5">
-          <button className="hover:text-blue-600 transition-colors">
-            Become Educator
-          </button>
-          <Link
-            to="/my-enrollments"
-            className="hover:text-blue-600 transition-colors"
-          >
-            My Enrollments
-          </Link>
+          {user && (
+            <>
+              <button className="hover:text-blue-600 transition-colors">
+                Become Educator
+              </button>
+              <Link
+                to="/my-enrollments"
+                className="hover:text-blue-600 transition-colors"
+              >
+                My Enrollments
+              </Link>
+            </>
+          )}
         </div>
         {user ? (
           <UserButton />
         ) : (
-          <button 
-          onClick={() => openSignIn()}
-          className="bg-blue-600 text-white px-6 py-2 rounded-full hover:bg-blue-700 transition-colors cursor-pointer">
+          <button
+            onClick={() => openSignIn()}
+            className="bg-blue-600 text-white px-6 py-2 rounded-full hover:bg-blue-700 transition-colors cursor-pointer"
+          >
             Create Account
           </button>
         )}
@@ -41,24 +46,26 @@ const Navbar = () => {
       {/* Mobile Navbar */}
       {/* Mobile Navbar */}
       <div className="md:hidden flex items-center gap-2 sm:gap-5 text-gray-500 text-sm">
-          <button className="hover:text-blue-600 transition-colors">
-            Become Educator
-          </button>
-          <Link
-            to="/my-enrollments"
-            className="hover:text-blue-600 transition-colors"
-          >
-            My Enrollments
-          </Link>
-          {user ? (
-            <UserButton />
-          ) : (
-            <button 
-            onClick={() => openSignIn()}
-            className="cursor-pointer">
-              <img src={assets.user_icon} alt="user icon" className="w-8 h-8" />
+        {user && (
+          <>
+            <button className="hover:text-blue-600 transition-colors">
+              Become Educator
             </button>
-          )}
+            <Link
+              to="/my-enrollments"
+              className="hover:text-blue-600 transition-colors"
+            >
+              My Enrollments
+            </Link>
+          </>
+        )}
+        {user ? (
+          <UserButton />
+        ) : (
+          <button onClick={() => openSignIn()} className="cursor-pointer">
+            <img src={assets.user_icon} alt="user icon" className="w-8 h-8" />
+          </button>
+        )}
       </div>
     </div>
   );
