982e5b8f90cc5849db0f3dfa8536a1c65b71d341
shanto <shantokumar22@gmail.com>
2026-01-31 23:25:41 +0600

    Add initial student Navbar component with logo and educator button.

--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { assets } from "../../assets/assets";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div>
+      <img
+        src={assets.logo}
+        alt="logo"
+        className="w-28 lg:h-32 cursor-pointer"
+      />
+      <div className="hidden md:flex items-center gap-5 gray-500">
+          <div>
+            <button className="">Become Educator</button>
+            <Link to="/login"></Link>
+          </div>
+      </div>
+      <div>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
