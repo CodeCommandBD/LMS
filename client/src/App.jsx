43766926322dd07bde96e3f4702d82622d74935d
shanto <shantokumar22@gmail.com>
2026-02-01 00:23:17 +0600

    set up initial client application structure including routing, student navbar, global styles, and a placeholder home page.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react' 
 import { Outlet } from 'react-router-dom'
+import Navbar from './components/student/Navbar'
 
 const App = () => {
   return (
-    <div>
-     <Outlet />
+    <div className='min-h-screen bg-white '>
+      <Navbar />
+      <Outlet />
     </div>
   )
 }
--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -3,21 +3,31 @@ import { assets } from "../../assets/assets";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const isCourseListPage = location.pathname.includes("/course-list");
   return (
-    <div className="flex items-center justify-between">
+    <div
+      className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 py-4 border-b border-gray-500 ${isCourseListPage ? "bg-white" : "bg-cyan-100/70"}`}
+    >
       <img
         src={assets.logo}
         alt="logo"
-        className="w-28 lg:h-32 cursor-pointer"
+        className="w-28 lg:w-32 cursor-pointer"
       />
-      <div className="hidden md:flex items-center gap-5 gray-500">
-          <div>
-            <button className="">Become Educator</button>
-            <Link to="/my-enrollments" className="">My Enrollments</Link>
-          </div>
-          <button className="bg-blue-600 text-white px-4 py-2 rounded-full">Create Account</button>
-      </div>
-      <div>
+      <div className="hidden md:flex items-center gap-5 text-gray-700">
+        <div className="flex items-center gap-5">
+          <button className="hover:text-blue-600 transition-colors">
+            Become Educator
+          </button>
+          <Link
+            to="/my-enrollments"
+            className="hover:text-blue-600 transition-colors"
+          >
+            My Enrollments
+          </Link>
+        </div>
+        <button className="bg-blue-600 text-white px-6 py-2 rounded-full hover:bg-blue-700 transition-colors">
+          Create Account
+        </button>
       </div>
     </div>
   );
