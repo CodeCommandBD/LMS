5024af0348caf2922ea220a6e0cb0c1281f755dc
shanto <shantokumar22@gmail.com>
2026-02-01 22:32:15 +0600

    create Hero component and integrate it into the student Home page.

--- client/src/components/student/Hero.jsx
+++ client/src/components/student/Hero.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { assets } from "../../assets/assets";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div className="flex flex-col items-center justify-center w-full md:pt-36 pt-20 px-7 md:px-0 space-y-7 text-center bg-gradient-to-b from-cyan-100/70">
+      <h1 className="relative md:text-5xl text-4xl font-bold max-w-3xlk mx-auto text-center text-gray-700">
+        Empower your future with the courses designed to{" "}
+        <span className="text-blue-600 font-semibold">fit your choice.</span>
+        <img
+          className="md:block hidden absolute right-0 -bottom-7"
+          src={assets.sketch}
+          alt="sketch"
+        />
+      </h1>
+      <p className="md:block text-gray-600 max-w-2xl mx-auto text-md">
+        We bring together world-class instructors, interactive content, and a
+        supportive community to help you achieve your personal and professional
+        goals.
+      </p>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
--- client/src/pages/student/Home.jsx
+++ client/src/pages/student/Home.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import Hero from "../../components/student/Hero"
 
 const Home = () => {
   return (
     <div>
+      <Hero/>
     </div>
   )
 }
