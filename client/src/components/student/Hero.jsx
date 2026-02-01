fa6a99dafc017a62464cc5d3f4e9295caccd1a52
shanto <shantokumar22@gmail.com>
2026-02-01 22:35:24 +0600

    Add a new `Hero` component for the student landing page.

--- client/src/components/student/Hero.jsx
+++ client/src/components/student/Hero.jsx
@@ -14,9 +14,10 @@ const Hero = () => {
         />
       </h1>
       <p className="md:block hidden text-gray-600 max-w-2xl mx-auto text-md">
-        We bring together world-class instructors, interactive content, and a
-        supportive community to help you achieve your personal and professional
-        goals.
+        We bring together world-class instructors, interactive content, and a supportive community to help you achieve your personal and professional goals.
+      </p>
+      <p className="md:hidden block text-gray-600 max-w-2xl mx-auto text-md">
+       We bring together world-class instructors to help you achieve your professional goals.
       </p>
     </div>
   );
